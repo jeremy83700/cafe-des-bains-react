e493556c371ec59e9c480cea1be8f9d91cc4a8bb
jérémy Six <jeremysixcontact@gmail.com>
2023-06-07 11:18:30 +0200

    Progression creation navbar >1280px

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,66 +1,76 @@
-
-import React from 'react';
+import React, { useState } from 'react';
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
-import Logo from '../img/logo/Logo cafe des bains blanc.webp';
+import { ThemeProvider } from '@mui/material/styles';
+import theme from '../theme.js';
+import Logo from '../img/logo/Logo cafe des bains vintage.webp';
+import LogoEcrit from '../img/logo/Logo cafe des bains ecriture.webp';
+import FlagEn from '../img/logo/flag en.webp';
+import FlagFr from '../img/logo/flag fr.webp';
 import './Navbar.css';
 
 
+const Navbar = ({ language, onToggleLanguage }) => {
+  const [currentLanguage, setCurrentLanguage] = useState(language);
+  const [flagIcon, setFlagIcon] = useState(FlagFr);
 
-import './Navbar.css';
+  const handleLanguageChange = () => {
+    if (currentLanguage === 'fr') {
+      setCurrentLanguage('en');
+      setFlagIcon(FlagEn);
+    } else {
+      setCurrentLanguage('fr');
+      setFlagIcon(FlagFr);
+    }
+    onToggleLanguage(); // Appeler la fonction onToggleLanguage après le changement de langue
+  };
 
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <header className='wrapper-menu'>
+          <div className="logo-navbar">
+            <img src={Logo} alt="logo cafe des bains" className="logo-img" />
+            <img src={LogoEcrit} alt="logo cafe des bains" className="logo-img2" />
+          </div>
 
-const Navbar = ({language, onToggleLanguage}) => {
+          <nav className="wrapper-nav">
+            <Stack spacing={4} direction="row">
+              <Button variant="text" size='large' color='orclair' sx={{ fontFamily: 'Neuton', fontSize: '1em' }}>
+                {currentLanguage === 'fr' ? 'Brasserie' : 'Brewery'}
+              </Button>
+              <Button variant="text" size='medium' color='orclair' sx={{ fontFamily: 'Neuton', fontSize: '1em' }}>
+                {currentLanguage === 'fr' ? 'La carte' : 'Menu'}
+              </Button>
+              <Button variant="text" size='medium' color='orclair' sx={{ fontFamily: 'Neuton', fontSize: '1em' }}>
+                {currentLanguage === 'fr' ? 'Concert' : 'Live Musik'}
+              </Button>
+              <Button variant="text" size='medium' color='orclair' sx={{ fontFamily: 'Neuton', fontSize: '1em'  }}>
+                Cocktails
+              </Button>
+            </Stack>
+          </nav>
 
-  return (<>
-    <header className='wrapper-menu'>
+          <div className="wrapper-language-reservation">
+            <Stack spacing={4} direction="row">
+              <Button variant="contained" color='orclair'  sx={{ fontFamily: 'Neuton', fontSize: '1em', color:"bleucafedesbains", padding:'0.5em 2em 0.5em 2em', margin: "2em" }} className='button-wrapper-reservation'>
+                {currentLanguage === 'fr' ? 'Réserver' : 'Booking'}
+              </Button>
+              <Button onClick={handleLanguageChange} >
+                <img src={flagIcon} alt="Language flag" className="button-wrapper-language"/>
+              </Button>
+            </Stack>
+          </div>
 
-      <div className="logo-navbar">
-
-        <img src={Logo} alt="logo cafe des bains" class="logo-img"/>
-
-      </div>
-
-      <div className="wrapper-menu">
-
-        <nav class="menu">
-          <ul>
-          <Stack spacing={4} direction="row">
-
-            <Button variant="text">{language === 'fr' ? 'Brasserie' : 'Brewery'}</Button>
-            <Button variant="text">{language === 'fr' ? 'La carte' : 'Menu'}</Button>
-            <Button variant="text">{language === 'fr' ? 'Concert' : 'Live Musik'}</Button>
-            <Button variant="text">Cocktails</Button>
-
-          </Stack>
-          </ul>
-        </nav>
-
-        
-      
-      </div>
-
-      <div className="wrapper-language-reservation">
-
-        <Button variant="contained" className='button-wrapper-reservation'>{language === 'fr' ? 'Réserver' : 'Booking'}</Button>
-        <Button onClick={onToggleLanguage} className="button-wrapper-language">Changer la langue</Button>
-
-      </div>
-
-      <div className="menu-burger">
-
-        <span></span>
-        <span></span>
-        <span></span>
-
-      </div>
-
-    </header>
-
-  </>
-    
-  )
-}
-
+          <div className="menu-burger">
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+        </header>
+      </ThemeProvider>
+    </>
+  );
+};
 
 export default Navbar;
