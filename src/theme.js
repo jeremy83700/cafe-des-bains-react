517bb9762b99c51233b99a651127540383db255b
jérémy Six <jeremysixcontact@gmail.com>
2023-06-07 11:19:06 +0200

    creation theme.js

---
+++ src/theme.js
@@ -0,0 +1,39 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    bleucafedesbains: {
+      main: '#1A2B3C',
+    },
+    blanc: {
+      main: '#fff',
+    },
+    blanccasse: {
+      main: '#F9F4E0',
+    },
+    rosepale: {
+      main: '#F7D7BF',
+    },
+    orfonce: {
+      main: '#A87827',
+    },
+    orclair: {
+      main: '#F5C169',
+    },
+    bleuclair: {
+      main: '#396897',
+    },
+    gris: {
+      main: '#566575',
+    },
+  },
+  typography: {
+    fontFamily: {
+      raleway: 'Raleway',
+      neuton: 'Neuton',
+      hind: 'Hind',
+    },
+  },
+});
+
+export default theme;
